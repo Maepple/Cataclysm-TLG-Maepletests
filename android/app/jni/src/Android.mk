a1ef501ea802f03a0335ec01db54707920c361f0
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2025-08-13 10:25:12 -0700

    Revert "Update Android.mk"
    This reverts commit e14c0e4443e7a871b1cf4506f4cdd811b1e0f8dd.

--- android/app/jni/src/Android.mk
+++ android/app/jni/src/Android.mk
@@ -10,9 +10,7 @@ LOCAL_CPP_FEATURES := exceptions rtti
 CATA_SRCS := $(sort $(wildcard $(LOCAL_PATH)/*.cpp))
 LOCAL_SRC_FILES := $(sort $(CATA_SRCS:$(LOCAL_PATH)/%=%))
 
-LOCAL_STATIC_LIBRARIES := third-party
-
-LOCAL_SHARED_LIBRARIES := libhidapi SDL2 SDL2_mixer SDL2_image SDL2_ttf mpg123
+LOCAL_SHARED_LIBRARIES := libhidapi SDL2 SDL2_mixer SDL2_image SDL2_ttf mpg123 third-party
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -lz
 
@@ -25,6 +23,6 @@ ifeq ($(OS),Windows_NT)
 	LOCAL_SHORT_COMMANDS := true
 endif
 
-include $(BUILD_SHARED_LIBRARY)
-
 include $(LOCAL_PATH)/../android/app/jni/src/third-party/Android.mk
+
+include $(BUILD_SHARED_LIBRARY)
